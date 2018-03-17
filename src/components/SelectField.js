51f1d1d2ba7db17782122cbdc2ce140a81e02b40
Brunno Vodola <bvodola@gmail.com>
2018-03-17 20:39:37 -0300

    Fixing select field

--- src/components/SelectField.js
+++ src/components/SelectField.js
@@ -7,8 +7,7 @@ const SelectField = (props) => {
   let { name, options, stateHandler } = props;
   let state = stateHandler.get();
   let value = _.get(state,name);
-  console.log('value', value);
-  let childProps = JSON.parse(JSON.stringify(props));
+  let childProps = Object.assign({}, props);
   delete childProps.stateHandler;
   delete childProps.options;
   delete childProps.onChange;
@@ -17,18 +16,18 @@ const SelectField = (props) => {
     <MuiSelect
       {...childProps}
       value={value}
-      onChange={(ev, i, value) =>  {
-        stateHandler.set(name, value);
+      onChange={(ev) =>  {
+        stateHandler.set(name, ev.target.value);
         if(typeof props.onChange === 'function')
-          props.onChange(ev,i,value);
+          props.onChange(ev);
       }}
     >
       {options.map((option, i) => (
         <MenuItem
           key={i}
-          value={ option.value || option._id || option.id || option.name || option.title }
+          value={ option.value || option._id || option.id || option.name || option.title || option}
         >
-          {option.label || option.name || option.title || option.value}
+          {option.label || option.name || option.title || option.value || option}
         </MenuItem>
       ))}
     </MuiSelect>
